14556bdd31dcdfc4f077175fc045ddbd2d5c2f1c
igorsimb <fatalhope@gmail.com>
2021-12-14 21:51:42 +0300

    Slight frontend text changes

--- home/models.py
+++ home/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 class Movie(models.Model):
     title = models.CharField('Название фильма', max_length=100)
-    gross = models.IntegerField('Сборы', help_text='По всему миру, в гривнах.')
+    gross = models.IntegerField('Сборы', help_text='По всему миру, в долларах.')
 
     def __str__(self):
         return self.title
